f5eb333b8593263b91242783c9aab28bc6bf0b92
cfalzone <cfalzone@aquent.com>
2014-03-31 09:27:50 -0400

    Added Migrations and User Table

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'cribbb',
+			'username'  => 'cribbb',
+			'password'  => 'cribbb',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
